2d0088852dd5599a6104f49536e94a518de99043
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-21 15:32:25 +0100

    add GameController with GET endpoints

---
+++ backend/src/main/java/com/github/rohkaz/controller/GameController.java
@@ -0,0 +1,33 @@
+package com.github.rohkaz.controller;
+
+import com.github.rohkaz.model.Game;
+import com.github.rohkaz.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+@RequiredArgsConstructor
+public class GameController {
+
+    private final GameService gameService;
+
+    @GetMapping("/games")
+    public List<Game> getAllGames() {
+        return gameService.getAllGames();
+    }
+
+    @GetMapping("/games/{id}")
+    Game getGameByID(@PathVariable String id) {
+        return gameService.getGameByID(id);
+    }
+
+}
+
+
+
