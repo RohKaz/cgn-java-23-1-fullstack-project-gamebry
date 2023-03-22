02bbc260235231b4e34faa08aa2e8dd88bdb331c
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-22 14:52:25 +0100

    style GameCard with MUI

--- frontend/src/component/GameCard.tsx
+++ frontend/src/component/GameCard.tsx
@@ -1,16 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import {CardActionArea} from '@mui/material';
 import {Game} from "../model/Game";
-import "./GameCard.css"
-
 
 type GameCardProps = {
     game: Game;
 }
-
 export default function GameCard(props: GameCardProps) {
+
     return (
-        <div className={"game-card"}>
-            <h1>{props.game.gameTitle}</h1>
-            <p>{props.game.gameDescription}</p>
-        </div>
-    )
+        <Card sx={{maxWidth: 345}}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image="/static/images/cards/contemplative-reptile.jpg"
+                    alt="An image of the game"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {props.game.gameTitle}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {props.game.gameDescription}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
 }
