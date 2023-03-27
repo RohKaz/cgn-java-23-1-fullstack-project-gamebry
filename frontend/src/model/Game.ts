c0c640bb4700e0d194f5059a79da7ce8967c101f
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-27 16:08:48 +0200

    add new properties for Game model

--- frontend/src/model/Game.ts
+++ frontend/src/model/Game.ts
@@ -2,4 +2,9 @@ export type Game = {
     gameId: string;
     gameTitle: string;
     gameDescription: string;
+    gameDeveloper: string;
+    gameReleaseDate: string;
+    gamePlatforms: string[];
+    gameGenres: string[];
+
 }
