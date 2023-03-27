bb3968a32a80cdcdf9db0c246c03b854ba9939fd
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-27 18:50:54 +0200

    remove Css file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
 import {Game} from "./model/Game";
 import axios from "axios";
 import GameCardGallery from "./component/GameCardGallery";
