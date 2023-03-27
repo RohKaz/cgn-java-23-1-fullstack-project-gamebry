6c49f3ddfd753207b0e710c49d4a3196ff1b3d21
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-27 18:14:15 +0200

    add compact expandable List for game information

--- frontend/src/component/GameCardDetailsPage.tsx
+++ frontend/src/component/GameCardDetailsPage.tsx
@@ -2,8 +2,10 @@ import {useEffect, useState} from "react";
 import {Game} from "../model/Game";
 import {useParams} from "react-router-dom";
 import axios from "axios";
-import {Box, Divider} from "@mui/material";
+import {Box, Collapse, Divider, List, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
 import Typography from "@mui/material/Typography";
+import AccessTime from "@mui/icons-material/AccessTime";
+import {Category, ExpandLess, ExpandMore, Info, SportsEsports, SupervisedUserCircle} from "@mui/icons-material";
 
 
 export default function GameCardDetailsPage() {
@@ -12,6 +14,10 @@ export default function GameCardDetailsPage() {
     const id: string | undefined = params.gameId;
 
     const [details, setDetails] = useState<Game | undefined>();
+    const [open, setOpen] = useState(true);
+    const handleClick = () => {
+        setOpen(!open);
+    }
 
     const requestURL: string = "/api/games/" + id
 
@@ -37,6 +43,7 @@ export default function GameCardDetailsPage() {
             textAlign: "left",
             padding: 6,
             backgroundColor: "#1f345c",
+
             //283561
             //1f2a4d
         }}>
@@ -46,18 +53,56 @@ export default function GameCardDetailsPage() {
                 textAlign: "left",
             }}>
                 <div className={"game-title"}>{details.gameTitle}</div>
-                <p>
-                    <Box component="img" sx={{
-                        imageRendering: "pixelated",
-                        height: 200,
-                        width: 300,
-                        display: "flex",
-                    }}
-                         alt={"Here should be a cover image of the game"}
-                         src={"http://1.bp.blogspot.com/-yjm45McKLL4/UH3VFZiKweI/AAAAAAAABaQ/mpeVscC0GiA/s1600/Skyrim+-+Cover.jpg"}
-                    />
-                </p>
-                <Typography sx={{fontWeight: "bold"}}>
+                <Box component="img" sx={{
+                    imageRendering: "pixelated",
+                    height: 200,
+                    width: 320,
+                    display: "flex",
+                    marginTop: 2,
+                }}
+                     alt={"Here should be a cover image of the game"}
+                     src={"http://1.bp.blogspot.com/-yjm45McKLL4/UH3VFZiKweI/AAAAAAAABaQ/mpeVscC0GiA/s1600/Skyrim+-+Cover.jpg"}
+                />
+                <List sx={{maxWidth: 400, height: "auto", color: "black", marginTop: 2,}}
+                      component={"nav"}>
+                    <ListItemButton onClick={handleClick}>
+                        <ListItemIcon>
+                            <Info/>
+                        </ListItemIcon>
+                        <ListItemText primary={"Game information"}/>
+                        {open ? <ExpandLess/> : <ExpandMore/>}
+                    </ListItemButton>
+                    <Divider/>
+                    <Collapse in={open} timeout={"auto"} unmountOnExit>
+                        <List component={"div"} disablePadding>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <AccessTime/>
+                                </ListItemIcon>
+                                <ListItemText primary={"Release date: " + details.gameReleaseDate}/>
+                            </ListItemButton>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <SupervisedUserCircle/>
+                                </ListItemIcon>
+                                <ListItemText primary={"Publisher: " + details.gameDeveloper}/>
+                            </ListItemButton>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <SportsEsports/>
+                                </ListItemIcon>
+                                <ListItemText primary={"Platforms: " + details.gamePlatforms}/>
+                            </ListItemButton>
+                            <ListItemButton>
+                                <ListItemIcon>
+                                    <Category/>
+                                </ListItemIcon>
+                                <ListItemText primary={"Genres: " + details.gameGenres}/>
+                            </ListItemButton>
+                        </List>
+                    </Collapse>
+                </List>
+                <Typography sx={{fontWeight: "bold", fontSize: 14, marginTop: 2}}>
                     About the game:
                 </Typography>
                 <Divider orientation={"horizontal"} sx={{border: 1, borderStyle: "", color: "black"}}/>
