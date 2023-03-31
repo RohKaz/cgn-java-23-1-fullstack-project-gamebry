a7c8ad02c885c28fc17a58b43dca42f2fb0097c6
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-03-31 17:55:06 +0200

    edit

--- backend/src/main/java/com/github/rohkaz/controller/GameController.java
+++ backend/src/main/java/com/github/rohkaz/controller/GameController.java
@@ -30,8 +30,10 @@ public class GameController {
         return rawgApiService.getGameById(id);
     }
 
-    @PostMapping("/games")
+    @PostMapping("/favorites")
     public Game addGame(@RequestBody GameDTO game) {
         return gameService.addGame(game);
     }
+
+
 }
