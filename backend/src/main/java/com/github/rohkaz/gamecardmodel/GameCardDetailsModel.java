4c76b4d5ed2b2fd6dad25ee4c930c9bdd2be916e
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-04-05 12:06:30 +0200

    edit model properties

--- backend/src/main/java/com/github/rohkaz/gamecardmodel/GameCardDetailsModel.java
+++ backend/src/main/java/com/github/rohkaz/gamecardmodel/GameCardDetailsModel.java
@@ -2,12 +2,17 @@ package com.github.rohkaz.gamecardmodel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public record GameCardDetailsModel(
 
         int id,
         @JsonProperty("name") String title,
         String description,
         @JsonProperty("released") String releaseDate,
+
+        List<Publisher> publishers,
+        List<Genre> genres,
         @JsonProperty("background_image") String cover,
         @JsonProperty("metacritic") int rating
 ) {
--- backend/src/main/java/com/github/rohkaz/gamecardmodel/GameCardModel.java
+++ backend/src/main/java/com/github/rohkaz/gamecardmodel/GameCardModel.java
@@ -2,10 +2,13 @@ package com.github.rohkaz.gamecardmodel;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.List;
+
 public record GameCardModel(
         int id,
         @JsonProperty("name") String title,
         @JsonProperty("released") String releaseDate,
+        List<Genre> genres,
         @JsonProperty("background_image") String cover,
         @JsonProperty("metacritic") int rating
 ) {
