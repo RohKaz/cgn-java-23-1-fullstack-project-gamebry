0530565a56007546f8b1b98bebc7468bfe51eb0d
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-04-12 14:18:36 +0200

    edit create method

--- backend/src/main/java/com/github/rohkaz/security/UserController.java
+++ backend/src/main/java/com/github/rohkaz/security/UserController.java
@@ -1,7 +1,10 @@
 package com.github.rohkaz.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -11,10 +14,35 @@ import java.util.List;
 public class UserController {
 
     private final AppUserRepository appUserRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @PostMapping
     public AppUser createNewUser(@RequestBody AppUser user) {
-        return appUserRepository.save(user);
+        if (user.username() == null || user.username().length() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Username is required");
+        }
+        if (user.password() == null || user.password().length() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password is required");
+        }
+        if (appUserRepository.existsByUsername(user.username())) {
+            throw new ResponseStatusException(HttpStatus.CONFLICT, "User already exists");
+        }
+
+        AppUser newUser = new AppUser(
+                user.id(),
+                user.username(),
+                user.password(),
+                "BASIC"
+        );
+
+        AppUser out = appUserRepository.save(newUser);
+
+        return new AppUser(
+                out.id(),
+                out.username(),
+                null,
+                out.role()
+        );
     }
 
     @GetMapping("all")
