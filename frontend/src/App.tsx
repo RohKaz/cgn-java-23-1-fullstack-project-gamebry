e41bdc0339baf137144c2dc220c229750a296665
Roh Kazimi <rohula-kazimi@hotmail.com>
2023-04-14 15:49:45 +0200

    edits

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,21 +5,11 @@ import GameCardGallery from "./component/GameCardGallery";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import GameCardDetailsPage from "./component/GameCardDetailsPage";
 import SignUpPage from "./component/SignUpPage";
-import Cookies from "js-cookie";
 import SignInPage from "./component/SignInPage";
 
 function App() {
 
     const [game, setGame] = useState<GameCardModel[]>([]);
-
-    axios.interceptors.request.use(function (config) {
-        return fetch("/api/csrf").then(() => {
-            config.headers["X-XSRF-TOKEN"] = Cookies.get("XSRF-TOKEN");
-            return config;
-        });
-    }, function (error) {
-        return Promise.reject(error);
-    });
 
     axios.interceptors.response.use(function (response) {
         return response;
@@ -29,7 +19,6 @@ function App() {
         }
         return Promise.reject(error);
     });
-
 
     function getNewAndUpcomingGames() {
         axios.get("/api/games/new")
